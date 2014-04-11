2ee81e41d4baff1ae518975cad0060d63e28fb98
Anibal Copitan <acopitan@gmail.com>
2014-04-11 00:13:03 -0500

    slqLite

--- application/config/database.php
+++ application/config/database.php
@@ -47,13 +47,12 @@
 
 $active_group = 'default';
 $active_record = TRUE;
-
-$db['default']['hostname'] = 'localhost';
+$db['default']['hostname'] = 'sqlite:'.APPPATH.'db/gym.sqlite';
 $db['default']['username'] = '';
 $db['default']['password'] = '';
 $db['default']['database'] = '';
-$db['default']['dbdriver'] = 'mysql';
-$db['default']['dbprefix'] = '';
+$db['default']['dbdriver'] = 'pdo';
+$db['default']['dbprefix'] = 'ac_';
 $db['default']['pconnect'] = TRUE;
 $db['default']['db_debug'] = TRUE;
 $db['default']['cache_on'] = FALSE;
