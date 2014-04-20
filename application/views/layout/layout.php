b43e1799b967c4432861869f6810e725ae5fae03
Anibal Copitan <acopitan@gmail.com>
2014-04-20 16:20:38 -0500

    layout update ()

--- application/views/layout/layout.php
+++ application/views/layout/layout.php
@@ -38,19 +38,19 @@
                     <div class="sidebar-shortcuts" id="sidebar-shortcuts">
                         <div class="sidebar-shortcuts-large" id="sidebar-shortcuts-large">
                             <button class="btn btn-success">
-                                <i class="icon-signal"></i>
+                                <i class="icon-signal_"></i>
                             </button>
 
                             <button class="btn btn-info">
-                                <i class="icon-pencil"></i>
+                                <i class="icon-pencil_"></i>
                             </button>
 
                             <button class="btn btn-warning">
-                                <i class="icon-group"></i>
+                                <i class="icon-group_"></i>
                             </button>
 
                             <button class="btn btn-danger">
-                                <i class="icon-cogs"></i>
+                                <i class="icon-cogs_"></i>
                             </button>
                         </div>
 
@@ -66,302 +66,20 @@
                     </div><!-- #sidebar-shortcuts -->
 
                     <ul class="nav nav-list">
-                        <li>
-                            <a href="index-2.html">
+                        <li class="active">
+                            <a href="/dashboard">
                                 <i class="icon-dashboard"></i>
-                                <span class="menu-text"> Dashboard </span>
+                                <span class="menu-text"> Principal </span>
                             </a>
                         </li>
 
                         <li>
-                            <a href="typography.html">
+                            <a href="#">
                                 <i class="icon-text-width"></i>
-                                <span class="menu-text"> Typography </span>
+                                <span class="menu-text"> Otro </span>
                             </a>
                         </li>
 
-                        <li>
-                            <a href="#" class="dropdown-toggle">
-                                <i class="icon-desktop"></i>
-                                <span class="menu-text"> UI Elements </span>
-
-                                <b class="arrow icon-angle-down"></b>
-                            </a>
-
-                            <ul class="submenu">
-                                <li>
-                                    <a href="elements.html">
-                                        <i class="icon-double-angle-right"></i>
-                                        Elements
-                                    </a>
-                                </li>
-
-                                <li>
-                                    <a href="buttons.html">
-                                        <i class="icon-double-angle-right"></i>
-                                        Buttons &amp; Icons
-                                    </a>
-                                </li>
-
-                                <li>
-                                    <a href="treeview.html">
-                                        <i class="icon-double-angle-right"></i>
-                                        Treeview
-                                    </a>
-                                </li>
-
-                                <li>
-                                    <a href="jquery-ui.html">
-                                        <i class="icon-double-angle-right"></i>
-                                        jQuery UI
-                                    </a>
-                                </li>
-
-                                <li>
-                                    <a href="nestable-list.html">
-                                        <i class="icon-double-angle-right"></i>
-                                        Nestable Lists
-                                    </a>
-                                </li>
-
-                                <li>
-                                    <a href="#" class="dropdown-toggle">
-                                        <i class="icon-double-angle-right"></i>
-
-                                        Three Level Menu
-                                        <b class="arrow icon-angle-down"></b>
-                                    </a>
-
-                                    <ul class="submenu">
-                                        <li>
-                                            <a href="#">
-                                                <i class="icon-leaf"></i>
-                                                Item #1
-                                            </a>
-                                        </li>
-
-                                        <li>
-                                            <a href="#" class="dropdown-toggle">
-                                                <i class="icon-pencil"></i>
-
-                                                4th level
-                                                <b class="arrow icon-angle-down"></b>
-                                            </a>
-
-                                            <ul class="submenu">
-                                                <li>
-                                                    <a href="#">
-                                                        <i class="icon-plus"></i>
-                                                        Add Product
-                                                    </a>
-                                                </li>
-
-                                                <li>
-                                                    <a href="#">
-                                                        <i class="icon-eye-open"></i>
-                                                        View Products
-                                                    </a>
-                                                </li>
-                                            </ul>
-                                        </li>
-                                    </ul>
-                                </li>
-                            </ul>
-                        </li>
-
-                        <li>
-                            <a href="#" class="dropdown-toggle">
-                                <i class="icon-list"></i>
-                                <span class="menu-text"> Tables </span>
-
-                                <b class="arrow icon-angle-down"></b>
-                            </a>
-
-                            <ul class="submenu">
-                                <li>
-                                    <a href="tables.html">
-                                        <i class="icon-double-angle-right"></i>
-                                        Simple &amp; Dynamic
-                                    </a>
-                                </li>
-
-                                <li>
-                                    <a href="jqgrid.html">
-                                        <i class="icon-double-angle-right"></i>
-                                        jqGrid plugin
-                                    </a>
-                                </li>
-                            </ul>
-                        </li>
-
-                        <li>
-                            <a href="#" class="dropdown-toggle">
-                                <i class="icon-edit"></i>
-                                <span class="menu-text"> Forms </span>
-
-                                <b class="arrow icon-angle-down"></b>
-                            </a>
-
-                            <ul class="submenu">
-                                <li>
-                                    <a href="form-elements.html">
-                                        <i class="icon-double-angle-right"></i>
-                                        Form Elements
-                                    </a>
-                                </li>
-
-                                <li>
-                                    <a href="form-wizard.html">
-                                        <i class="icon-double-angle-right"></i>
-                                        Wizard &amp; Validation
-                                    </a>
-                                </li>
-
-                                <li>
-                                    <a href="wysiwyg.html">
-                                        <i class="icon-double-angle-right"></i>
-                                        Wysiwyg &amp; Markdown
-                                    </a>
-                                </li>
-
-                                <li>
-                                    <a href="dropzone.html">
-                                        <i class="icon-double-angle-right"></i>
-                                        Dropzone File Upload
-                                    </a>
-                                </li>
-                            </ul>
-                        </li>
-
-                        <li>
-                            <a href="widgets.html">
-                                <i class="icon-list-alt"></i>
-                                <span class="menu-text"> Widgets </span>
-                            </a>
-                        </li>
-
-                        <li>
-                            <a href="calendar.html">
-                                <i class="icon-calendar"></i>
-
-                                <span class="menu-text">
-                                    Calendar
-                                    <span class="badge badge-transparent tooltip-error" title="2&nbsp;Important&nbsp;Events">
-                                        <i class="icon-warning-sign red bigger-130"></i>
-                                    </span>
-                                </span>
-                            </a>
-                        </li>
-
-                        <li>
-                            <a href="gallery.html">
-                                <i class="icon-picture"></i>
-                                <span class="menu-text"> Gallery </span>
-                            </a>
-                        </li>
-
-                        <li>
-                            <a href="#" class="dropdown-toggle">
-                                <i class="icon-tag"></i>
-                                <span class="menu-text"> More Pages </span>
-
-                                <b class="arrow icon-angle-down"></b>
-                            </a>
-
-                            <ul class="submenu">
-                                <li>
-                                    <a href="profile.html">
-                                        <i class="icon-double-angle-right"></i>
-                                        User Profile
-                                    </a>
-                                </li>
-
-                                <li>
-                                    <a href="inbox.html">
-                                        <i class="icon-double-angle-right"></i>
-                                        Inbox
-                                    </a>
-                                </li>
-
-                                <li>
-                                    <a href="pricing.html">
-                                        <i class="icon-double-angle-right"></i>
-                                        Pricing Tables
-                                    </a>
-                                </li>
-
-                                <li>
-                                    <a href="invoice.html">
-                                        <i class="icon-double-angle-right"></i>
-                                        Invoice
-                                    </a>
-                                </li>
-
-                                <li>
-                                    <a href="timeline.html">
-                                        <i class="icon-double-angle-right"></i>
-                                        Timeline
-                                    </a>
-                                </li>
-
-                                <li>
-                                    <a href="login.html">
-                                        <i class="icon-double-angle-right"></i>
-                                        Login &amp; Register
-                                    </a>
-                                </li>
-                            </ul>
-                        </li>
-
-                        <li class="active open">
-                            <a href="#" class="dropdown-toggle">
-                                <i class="icon-file-alt"></i>
-
-                                <span class="menu-text">
-                                    Other Pages
-                                    <span class="badge badge-primary ">5</span>
-                                </span>
-
-                                <b class="arrow icon-angle-down"></b>
-                            </a>
-
-                            <ul class="submenu">
-                                <li>
-                                    <a href="faq.html">
-                                        <i class="icon-double-angle-right"></i>
-                                        FAQ
-                                    </a>
-                                </li>
-
-                                <li>
-                                    <a href="error-404.html">
-                                        <i class="icon-double-angle-right"></i>
-                                        Error 404
-                                    </a>
-                                </li>
-
-                                <li>
-                                    <a href="error-500.html">
-                                        <i class="icon-double-angle-right"></i>
-                                        Error 500
-                                    </a>
-                                </li>
-
-                                <li>
-                                    <a href="grid.html">
-                                        <i class="icon-double-angle-right"></i>
-                                        Grid
-                                    </a>
-                                </li>
-
-                                <li class="active">
-                                    <a href="blank.html">
-                                        <i class="icon-double-angle-right"></i>
-                                        Blank Page
-                                    </a>
-                                </li>
-                            </ul>
-                        </li>
                     </ul><!-- /.nav-list -->
 
                     <div class="sidebar-collapse" id="sidebar-collapse">
@@ -472,6 +190,10 @@
             </a>
         </div><!-- /.main-container -->
         <?php require 'layout-partial/require_js.php';?>
+        <script type="text/javascript">            
+            ace.settings.main_container_fixed(true); // div right  
+            ace.settings.sidebar_collapsed(false); // div left
+        </script>
 
     </body>
 </html>
