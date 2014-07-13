50824544d005c63cd3c4f23108332147c56ffc5f
anibal copitan <detops.tk@gmail.com>
2014-07-13 00:40:45 -0500

    helper date

---
+++ application/helpers/MY_date_helper.php
@@ -0,0 +1,106 @@
+<?php
+if ( ! function_exists('date_add')){
+     function date_add($givendate,$day=0,$mth=0,$yr=0) {
+         $cd = strtotime($givendate);
+         return date('Y-m-d', mktime(date('h',$cd), date('i',$cd), date('s',$cd), date('m',$cd)+$mth,  date('d',$cd)+$day, date('Y',$cd)+$yr));
+     }
+}
+/* calcular la diferencia entre dos fechas */
+if ( ! function_exists('date_diff')){
+     function date_diff($start_date,$end_date,$format = 'd'){
+         $start_date = strtotime($start_date);
+         $end_date = strtotime($end_date);
+
+         switch ($format)
+         {
+            //seconds
+            case "s":
+                return floor(($end_date-$start_date));
+            //minutes
+            case "i":
+                 return floor(($end_date-$start_date)/60);
+            //hours
+            case "h":
+                 return floor(($end_date-$start_date)/3600);
+            //days
+            case "d":
+                 return floor(($end_date-$start_date)/86400);
+            //months
+            case "m":
+                 return floor(($end_date-$start_date)/2628000);
+            //years
+            case "y":
+                 return floor(($end_date-$start_date)/31536000);
+            //days
+            default:
+                 return floor(($end_date-$start_date)/86400);
+            }
+     }
+}
+/* conocer la hora exacta de un determinado timezone */
+if ( ! function_exists('get_date')){
+     function get_date($timezone = 'America/New_York', $full_date_time = false){
+           date_default_timezone_set($timezone);
+           $date = ($full_date_time) ? date('D,F j, Y, h:i:s A') : date('Y-m-d');
+           date_default_timezone_set('UTC');
+           return $date;
+     }
+}
+if ( ! function_exists('mysql_to_human')){
+     function mysql_to_human($fecha_machine){
+          $arrfecha = explode("-",trim($fecha_machine));
+          $resultado = $arrfecha[2]."/".$arrfecha[1]."/".$arrfecha[0];
+          return $resultado;
+     }
+}
+if ( ! function_exists('human_to_mysql')){
+     function human_to_mysql($fecha_human){// 12//05/1979
+          $arrfecha = explode("/", trim($fecha_human));
+          $resultado = $arrfecha[2]."-".$arrfecha[1]."-".$arrfecha[0];
+          return $resultado;
+     }
+}
+if ( ! function_exists('dia_semana')){
+    function dia_semana($dia_semana) {
+        /*$dias = array('Domingo', 'Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado');
+        return $dias[date("w", mktime(0, 0, 0, $mes, $dia, $ano))];*/
+         $dia_semana=(int) $dia_semana;
+         $dia_sema_textual='';
+
+         switch($dia_semana){
+             case 0: $dia_sema_textual='Domingo'; break;
+             case 1: $dia_sema_textual='Lunes'; break;
+             case 2: $dia_sema_textual='Martes'; break;
+             case 3: $dia_sema_textual='Miércoles'; break;
+             case 4: $dia_sema_textual='Jueves'; break;
+             case 5: $dia_sema_textual='Viernes'; break;
+             case 6: $dia_sema_textual='Sabado'; break;
+             default : $dia_sema_textual=''; break;
+         }
+         return $dia_sema_textual;
+    }
+}
+if ( ! function_exists('mes_textual')){
+    function mes_textual($mes=0)
+    {    $mes=(int) $mes;
+         $mes_textual='';
+
+         switch($mes){
+             case 1: $mes_textual='Enero'; break;
+             case 2: $mes_textual='Febrero'; break;
+             case 3: $mes_textual='Marzo'; break;
+             case 4: $mes_textual='Abril'; break;
+             case 5: $mes_textual='Mayo'; break;
+             case 6: $mes_textual='Junio'; break;
+             case 7: $mes_textual='Julio'; break;
+             case 8: $mes_textual='Agosto'; break;
+             case 9: $mes_textual='Setiembre'; break;
+             case 10: $mes_textual='Octubre'; break;
+             case 11: $mes_textual='Noviembre'; break;
+             case 12: $mes_textual='Diciembre'; break;
+             default : $mes_textual=''; break;
+         }
+         return $mes_textual;
+    }
+}
+?>
--- application/views/dashboard/index/_suscribirse.php
+++ application/views/dashboard/index/_suscribirse.php
@@ -1,4 +1,4 @@
-<form action="socio/add" name="formSuscribirse" id="formSuscribirse" method="POST">
+<form action="socio/suscripcion" name="formSuscribirse" id="formSuscribirse" method="POST">
     <fieldset name="fset_formSuscribirse" id="fset_formSuscribirse">
         <div class="modal-header">
             <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
@@ -13,18 +13,18 @@
             <div class="row form-group">
                 <div class="col-md-4"><label>Fecha de inicio</label></div>
                 <div class="col-md-8 input-group input-group-sm">
-                    <input type="text" id="suscrip_fecha_inicio" name="suscrip_fecha_inicio" value="<?php echo date("d\/m\/Y") ?>" class="required form-control">
+                    <input type="text" id="suscrip_fecha_inicio" name="suscrip_fecha_inicio" value="<?php echo date('d/m/Y') ?>" class="required form-control">
                     <span class="input-group-addon"><i class="icon-calendar"></i></span>
                 </div>
             </div>
 
-            <div class="row form-group">
+            <!--<div class="row form-group">
                 <div class="col-md-4"><label>Fecha fin</label></div>
                 <div class="col-md-8 input-group input-group-sm">
                     <input type="text" id="suscrip_fecha_fin" name="suscrip_fecha_fin" value="" class="required form-control" readonly="readonly">
                     <span class="input-group-addon"><i class="icon-calendar"></i></span>
                 </div>
-            </div>
+            </div>-->
 
         </div>
     </fieldset>
